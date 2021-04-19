ef484002efdc466195f130cbbfdfefc105c46a93
Hye Jeong <hye.93@hotmail.com>
2021-04-20 03:21:25 +0900

    멤버 서비스와 DAO 생성 - 정혜
    멤버 서비스와 DAO 생성 - 정혜

--- happyProject/src/com/kh/member/model/dao/MemberDao.java
+++ happyProject/src/com/kh/member/model/dao/MemberDao.java
@@ -1,5 +1,85 @@
 package com.kh.member.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.kh.common.JDBCTemplate.*;
+import com.kh.member.model.vo.Member;
+
 public class MemberDao {
+	
+	private Properties prop = new Properties();
+	
+	// 기본생성자
+	public MemberDao() {
+		
+		String fileName = MemberDao.class.getResource("/sql/member/member-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Member loginMember(Connection conn, String userId, String userPwd) {
+		// select문 => ResultSet객체 (한행) => Member객체
+		Member m = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("loginMember"); // 미완성된 sql문
+		
+		try {
+			pstmt = conn.prepareStatement(sql); // 미완성된sql문 담음
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userPwd);
+			
+			// 실행
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				m = new Member(rset.getInt("mem_no"),
+							   rset.getString("mem_id"),
+							   rset.getString("mem_pwd"),
+							   rset.getString("mem_name"),
+							   rset.getString("mem_address"),
+							   rset.getString("mem_phone"),
+							   rset.getString("nickname"),
+							   rset.getString("email"),
+							   rset.getString("email_yn"),
+							   rset.getString("enroll_route"),
+							   rset.getDate("enroll_date"),
+							   rset.getDate("recent_date"),
+							   rset.getString("admin_yn"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return m;
+	}
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
--- happyProject/src/com/kh/member/model/service/MemberService.java
+++ happyProject/src/com/kh/member/model/service/MemberService.java
@@ -1,5 +1,35 @@
 package com.kh.member.model.service;
 
-public class MemberService {
+import static com.kh.common.JDBCTemplate.*;
 
+import java.sql.Connection;
+
+import com.kh.member.model.dao.MemberDao;
+import com.kh.member.model.vo.Member;
+
+public class MemberService {
+	
+
+	public Member loginMember(String userId, String userPwd) {
+		
+		Connection conn = getConnection();
+		
+		Member loginMember = new MemberDao().loginMember(conn, userId, userPwd);
+		
+		close(conn);
+		
+		return loginMember;
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
