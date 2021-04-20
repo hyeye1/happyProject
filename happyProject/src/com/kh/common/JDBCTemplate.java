c671f938e6985814de180269d73ecfdfa2289e42
mmnn323 <mmnn323@naver.com>
2021-04-20 16:16:37 +0900

    Update JDBCTemplate.java

--- happyProject/src/com/kh/common/JDBCTemplate.java
+++ happyProject/src/com/kh/common/JDBCTemplate.java
@@ -11,14 +11,12 @@ import java.util.Properties;
 
 public class JDBCTemplate {
 	
-	// Connection 객체 생성(DB접속) 한 후 해당 Connection객체를 반환해주는 getConnection메소드
 	public static Connection getConnection() {
 		
 		Properties prop = new Properties();
 		
 		// 읽어들이고자 하는 driver.properties 파일의 물리적인 경로
 		String fileName = JDBCTemplate.class.getResource("/sql/driver/driver.properties").getPath();
-		// "C:\\webserver-workspace2\jspProject\webContent\WEB-INF\sql\driver\driver.properties"
 		
 		try {
 			prop.load(new FileInputStream(fileName));
@@ -28,9 +26,7 @@ public class JDBCTemplate {
 		Connection conn = null;
 		
 		try {
-			// 1) jdbc driver등록
 			Class.forName(prop.getProperty("driver"));
-			// 2) Connection 생셩(DB와 접속)
 			conn = DriverManager.getConnection(prop.getProperty("url"),
 											   prop.getProperty("username"),
 											   prop.getProperty("password"));
@@ -45,7 +41,6 @@ public class JDBCTemplate {
 	
 	
 	
-	// 전달받은 JDBC 용 객체 반납 해주는 메소드
 	public static void close(Connection conn) {
 		
 		try {
@@ -79,7 +74,6 @@ public class JDBCTemplate {
 	
 	
 	
-	// 전달받은 Connection을 가지고 트랜잭션 처리 메소드
 	
 	public static void commit(Connection conn) {
 		try {
