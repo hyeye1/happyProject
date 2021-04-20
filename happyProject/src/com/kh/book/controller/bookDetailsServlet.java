9b28a2c799d3d9d66810c23abd58fb5a3a94ff1d
mmnn323 <mmnn323@naver.com>
2021-04-20 20:08:14 +0900

    ㅁㄴㅁㄴㅁㄴ

--- happyProject/src/com/kh/book/controller/bookDetailsServlet.java
+++ happyProject/src/com/kh/book/controller/bookDetailsServlet.java
@@ -27,6 +27,10 @@ public class bookDetailsServlet extends HttpServlet {
     */
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       request.getRequestDispatcher("views/book/bookDetails.jsp").forward(request, response);
+      
+      /*
+       * *asgasdgsadg
+       */
    }
 
    /**
