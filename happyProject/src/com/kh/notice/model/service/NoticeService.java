447db34066515cd0a31896a2343930594046fa33
mmnn323 <mmnn323@naver.com>
2021-04-21 22:36:01 +0900

    ㅁㅁㅁ

--- happyProject/src/com/kh/notice/model/service/NoticeService.java
+++ happyProject/src/com/kh/notice/model/service/NoticeService.java
@@ -38,5 +38,19 @@ public class NoticeService {
 		close(conn);
 		return list;
 	}
+	
+	public int insertNotice(Notice n) {
+		Connection conn = getConnection();
+		int result = new NoticeDao().insertNotice(conn, n);
+		
+		if(result>0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		return result;
+	}
 
 }
