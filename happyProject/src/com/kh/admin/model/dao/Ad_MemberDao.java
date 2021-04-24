5817cecb332661882e63f4eee02c4a3866152fd1
yoo_jean <u_z1nee@naver.com>
2021-04-24 20:48:54 +0900

    회원

--- happyProject/src/com/kh/admin/model/dao/Ad_MemberDao.java
+++ happyProject/src/com/kh/admin/model/dao/Ad_MemberDao.java
@@ -6,15 +6,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Properties;
+
+import com.kh.admin.model.vo.Ad_Member;
+import com.kh.book.model.vo.Book;
+import com.kh.member.model.vo.Member;
 
 import static com.kh.common.JDBCTemplate.*;
 
 public class Ad_MemberDao {
 	private Properties prop = new Properties();
 	public Ad_MemberDao() {
-		String fileName = Ad_MemberDao.class.getResource("sql/member/member-mapper.xml").getPath(); // 절대경로 담음
-		
+		String fileName = Ad_MemberDao.class.getResource("sql/ad_member/ad_member-mapper.xml").getPath(); // 절대경로 담음
+
 		try {
 			prop.loadFromXML(new FileInputStream(fileName)); // 물리적 경
 		} catch (IOException e) {
@@ -49,6 +54,43 @@ public class Ad_MemberDao {
 		return listCount; // 총 게시글 반환 
 		
 		
+	}
+
+	public ArrayList<Ad_Member> selectList(Connection conn) {
+
+		PreparedStatement pstmt = null;
+		ArrayList<Ad_Member> list = new ArrayList<>();
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("selectList"); 
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				
+				list.add(new Ad_Member(rset.getInt("mem_no"),
+						   rset.getString("mem_id"),
+						   rset.getString("mem_pwd"),
+						   rset.getString("mem_name"),
+						   rset.getString("mem_address"),
+						   rset.getString("mem_phone"),
+						   rset.getString("email"),
+						   rset.getString("email_yn"),
+						   rset.getString("enroll_route"),
+						   rset.getDate("enroll_date"),
+						   rset.getDate("recent_login"),
+						   rset.getString("admin_yn")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return list;
 	}
 
 }
--- happyProject/src/com/kh/admin/model/service/Ad_MemberService.java
+++ happyProject/src/com/kh/admin/model/service/Ad_MemberService.java
@@ -3,8 +3,11 @@ package com.kh.admin.model.service;
 import static com.kh.common.JDBCTemplate.*;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import com.kh.admin.model.dao.Ad_MemberDao;
+import com.kh.admin.model.vo.Ad_Member;
+import com.kh.common.model.vo.PageInfo;
 
 public class Ad_MemberService {
 	
@@ -15,6 +18,15 @@ public class Ad_MemberService {
 		close(conn);
 		
 		return listCount;
+	}
+
+	public ArrayList<Ad_Member> selectList(PageInfo pi) {
+		Connection conn = getConnection();
+		ArrayList<Ad_Member> list = new Ad_MemberDao().selectList(conn);
+		
+		close(conn);
+		
+		return list;
 	}
 
 }
