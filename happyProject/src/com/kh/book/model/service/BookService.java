d46f2e77a9d5e21f37716b6776da55b1007555b8
anseoun <bazaar9095@gmail.com>
2021-04-26 20:25:09 +0900

    1

--- happyProject/src/com/kh/book/model/service/BookService.java
+++ happyProject/src/com/kh/book/model/service/BookService.java
@@ -64,47 +64,9 @@ public class BookService {
    }
    
    
-}
 
-	public ArrayList<Book> selectBookList(int bookNo){
-		
-		Connection conn = getConnection();
-		
-		ArrayList<Book> list = new BookDao().selectBookList(conn, bookNo);
-		
-		close(conn);
-		
-		return list;
-	}
-	
-	public Book bookDetail(int bookNo) {
-		
-		Connection conn = getConnection();
-		Book b = new BookDao().bookDetail(conn, bookNo);
-		
-		close(conn);
-		return b;
-		
-	}
-	
-	public Image bookInfoImg(int bookNo) {
-		
-		Connection conn = getConnection();
-		Image i = new BookDao().bookInfoImg(conn, bookNo);
-		
-		close(conn);
-		return i;
-		
-	}
-	
-	public ArrayList<Book> bestBookList(){
-		Connection conn = getConnection();
-		ArrayList<Book> list = new BookDao().bestBookList(conn);
-		close(conn);
-		return list;
-	}
 
-	
+
 	
 }
 
