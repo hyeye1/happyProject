2dc644909eabd8c6181452e996b53ffaae0e04f6
anseoun <bazaar9095@gmail.com>
2021-04-26 20:13:57 +0900

    dd

--- happyProject/src/com/kh/book/model/service/BookService.java
+++ happyProject/src/com/kh/book/model/service/BookService.java
@@ -12,56 +12,55 @@ import com.kh.book.model.vo.Image;
 import com.kh.member.model.vo.Member;
 
 public class BookService {
+   
+   public ArrayList<Book> selectBookList(int bookNo){
+      
+      Connection conn = getConnection();
+      
+      ArrayList<Book> list = new BookDao().selectBookList(conn, bookNo);
+      
+      close(conn);
+      
+      return list;
+   }
+   
+   public Book bookDetail(int bookNo) {
+      
+      Connection conn = getConnection();
+      Book b = new BookDao().bookDetail(conn, bookNo);
+      
+      close(conn);
+      return b;
+      
+   }
+   
+   public Image bookInfoImg(int bookNo) {
+      
+      Connection conn = getConnection();
+      Image i = new BookDao().bookInfoImg(conn, bookNo);
+      
+      close(conn);
+      return i;
+      
+   }
+   
+   public ArrayList<Book> bestBookList(){
+      Connection conn = getConnection();
+      ArrayList<Book> list = new BookDao().bestBookList(conn);
+      close(conn);
+      return list;
+   }
+   
+   public Member loginMem(int memNo) {
 	
-	public ArrayList<Book> selectBookList(int bookNo){
-		
-		Connection conn = getConnection();
-		
-		ArrayList<Book> list = new BookDao().selectBookList(conn, bookNo);
-		
-		close(conn);
-		
-		return list;
-	}
-	
-	public Book bookDetail(int bookNo) {
-		
-		Connection conn = getConnection();
-		Book b = new BookDao().bookDetail(conn, bookNo);
-		
-		close(conn);
-		return b;
-		
-	}
-	
-	public Image bookInfoImg(int bookNo) {
-		
-		Connection conn = getConnection();
-		Image i = new BookDao().bookInfoImg(conn, bookNo);
-		
-		close(conn);
-		return i;
-		
-	}
-
-	public Member loginMem(int memNo) {
-		
-		// 장바구니에 책을 담는 로그인한회원의 번호넘기기
-		Connection conn = getConnection();
-		Member m = new BookDao().loginMem(conn, memNo);
-		
-		close(conn);
-		return m;
-		
-
-	public ArrayList<Book> bestBookList(int rNum){
-		Connection conn = getConnection();
-		ArrayList<Book> list = new BookDao().bestBookList(conn, rNum);
-		close(conn);
-		return list;
-
-	}
-
-	
-	
+	   // 상세페이지에서 장바구니 이용하는 회원 번호
+	   Connection conn = getConnection();
+	   Member m = new BookDao().loginMem(conn, memNo);
+	   
+	   close(conn);
+	   return m;
+	   
+   }
+   
+   
 }
