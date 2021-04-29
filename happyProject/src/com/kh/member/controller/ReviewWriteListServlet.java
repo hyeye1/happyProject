09b23192b75ce550fa19cae39d8b35f8c18abb9c
SUMIN <smjm02195@naver.com>
2021-04-29 14:15:17 +0900

    myreview...폴더...

--- happyProject/src/com/kh/member/controller/ReviewWriteListServlet.java
+++ happyProject/src/com/kh/member/controller/ReviewWriteListServlet.java
@@ -1,11 +1,18 @@
 package com.kh.member.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.book.model.service.ReviewService;
+import com.kh.book.model.vo.Review;
+import com.kh.member.model.service.MyReviewService;
+import com.kh.member.model.vo.MyReview;
 
 /**
  * Servlet implementation class reviewWriteListViewServlet
@@ -26,6 +33,12 @@ public class ReviewWriteListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<MyReview> list = new MyReviewService().selectMyReview();
+		request.setAttribute("list", list);
+		//System.out.println(list);
+		
+		
 		request.getRequestDispatcher("views/member/reviewWriteListView.jsp").forward(request, response);
 	}
 
